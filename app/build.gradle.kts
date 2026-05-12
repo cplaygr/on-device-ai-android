1acd29a612f2077d07714e5856e05e2f01c468ed
jhjung <jhjung@tjmedia.com>
2026-05-12 14:12:58 +0900

    refactor: build.gradle 버전카탈로그 방식으로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.compose.material:material-icons-core")
+    implementation(libs.androidx.compose.material.icons.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -61,7 +61,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
 
-    implementation("com.google.mediapipe:tasks-genai:0.10.14")
+    implementation(libs.mediapipe.tasks.genai)
     implementation(libs.kotlinx.serialization.json)
 
 }
